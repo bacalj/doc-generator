f7229150b948d3cb3291bdc8cc891edf4644f08c
Joe Bacal <bacalj@gmail.com>
2023-10-11 22:23:47 -0400

    file and accompanying csv generator

--- docMaker.js
+++ docMaker.js
@@ -1,40 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { nanoid } from 'nanoid';
-
-const oceanSentences = [
-  "The ocean is vast and mysterious",
-  "The waves crash against the shore",
-  "The ocean is home to many creatures",
-  "The saltwater stings my eyes",
-  "The ocean is a beautiful shade of blue",
-  "The sound of the ocean is soothing",
-  "The ocean is a source of life and inspiration",
-  "The ocean is a powerful force of nature",
-  "The ocean is full of surprises and wonders",
-  "The ocean is a place of peace and tranquility"
-];
-
-const spaceSentences = [
-  "The universe is vast and mysterious",
-  "The stars twinkle in the night sky",
-  "Space is home to many celestial bodies",
-  "The vacuum of space is deadly to humans",
-  "The Milky Way is a beautiful sight to behold",
-  "The silence of space is eerie",
-  "Space exploration is a source of inspiration",
-  "The power of a black hole is immense",
-  "Space is full of surprises and wonders",
-  "The view of Earth from space is breathtaking"
-];
-
-const animalSentences = [
-  "The elephant is the largest land animal",
-  "The cheetah is the fastest land animal",
-  "The blue whale is the largest animal on Earth",
-  "The peregrine falcon is the fastest animal on Earth",
-  "The honey badger is known for its toughness"
-];
+import { oceanSentences, spaceSentences, animalSentences } from './sentences.js';
 
 const allSentences = [...oceanSentences, ...spaceSentences, ...animalSentences];
 
@@ -50,12 +17,39 @@ function makeTheFiles(){
   allSentences.forEach((sentence, i) => {
     const jsonString = getJsonForStr(sentence);
     const txtFileName = `file${i}.txt`;
-    fs.writeFileSync(path.join(__dirname, 'files', txtFileName), jsonString);
+    const dirPath = path.join(path.dirname(new URL(import.meta.url).pathname), 'files');
+    fs.writeFile(path.join(dirPath, txtFileName), jsonString, (err) => {
+      if (err) throw err;
+    });
   });
+}
+
+function getTag(i){
+  if (i < 10) return 'ocean';
+  if (i >= 10 && i < 20) return 'space';
+  if (i >= 20) return 'animal';
+}
+
+function createCSVFile(){
+  const csvFileName = 'schema.csv';
+  const dirPath = path.join(path.dirname(new URL(import.meta.url).pathname), 'files');
+  const csvFilePath = path.join(dirPath, csvFileName);
+  const writeStream = fs.createWriteStream(csvFilePath);
+
+  writeStream.write('fileName,words,tag\n');
+  allSentences.forEach((sentence, i) => {
+    const fileNameStr = `file${i}.txt`;
+    const tag = getTag(i);
+    writeStream.write(`${fileNameStr},${sentence},${tag}\n`);
+  });
+
+  writeStream.end();
 }
 
 function run(){
   makeTheFiles();
+  createCSVFile();
+  console.log("Done!");
 }
 
 run();
---
+++ sentences.js
@@ -0,0 +1,38 @@
+export const oceanSentences = [
+  "The ocean is vast and mysterious",
+  "The waves crash against the shore",
+  "The ocean is home to many creatures",
+  "The saltwater stings my eyes",
+  "The ocean is a beautiful shade of blue",
+  "The sound of the ocean is soothing",
+  "The ocean is a source of life and inspiration",
+  "The ocean is a powerful force of nature",
+  "The ocean is full of surprises and wonders",
+  "The ocean is a place of peace and tranquility"
+];
+
+export const spaceSentences = [
+  "The universe is vast and mysterious",
+  "The stars twinkle in the night sky",
+  "Space is home to many celestial bodies",
+  "The vacuum of space is deadly to humans",
+  "The Milky Way is a beautiful sight to behold",
+  "The silence of space is eerie",
+  "Space exploration is a source of inspiration",
+  "The power of a black hole is immense",
+  "Space is full of surprises and wonders",
+  "The view of Earth from space is breathtaking"
+];
+
+export const animalSentences = [
+  "The elephant is the largest land animal",
+  "The cheetah is the fastest land animal",
+  "The blue whale is the largest animal on Earth",
+  "The peregrine falcon is the fastest animal on Earth",
+  "The honey badger is known for its toughness",
+  "The giraffe is the tallest land animal",
+  "The kangaroo can jump up to three times its own height",
+  "The octopus has three hearts",
+  "The koala sleeps for up to 20 hours a day",
+  "The crocodile has the strongest bite of any animal"
+];
